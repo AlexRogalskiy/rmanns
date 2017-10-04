53338068f9a016a2a24554ed57d48dc0703ff225
Likid Geimfari <likid.geimfari@gmail.com>
2017-10-04 14:32:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+<a href="https://github.com/lk-geimfari/rmanns/">
+    <p align="center">
+      <img src="https://raw.githubusercontent.com/lk-geimfari/rmanns/master/logo.png">
+    </p>
+</a>
+
+
+Always before i buy books i download it from internet and read the samples. 
+Yeah, maybe it's not cool, but i don't want pay for shitty book. 
+The problems with books from the internets is annotations. Maybe they irritate only me. 
+Anyway, i have write this simple wrapper around pdftk for removing annotations from the books from 
+the popular pirate web sites, such as [www.it-ebooks.info ](www.it-ebooks.info ) and [www.allitebooks.com](www.allitebooks.com).
+
+
+## How to use
+
+As i say, this script is just a wrapper around `pdftk` and it's mean that you should install `pdftk`:
+
+```
+➜ sudo apt update
+➜ sudo apt install pdftk
+```
+
+Now you can use this script. You should move all your books with annotations to one folder and run script:
+```
+➜ chmod u+x rmanns.py
+➜ python3 rmanns.py
+
+Are you sure? [y/n]: y
+Annotations removed from file: Learning_Python.pdf.
+Annotations removed from file: Introducing_Data_Science.pdf.
+```
+
+Annotation will be deleted from all files that are in the current directory.
+
+
+You can simply add alias for this script to your `~/.bashrc` or `~/.zshrc` file:
+
+```bash
+alias rmanns="python3 ~/.rmanns.py"
+```
+
+## License
+MIT License
---
+++ rmanns.py
@@ -0,0 +1,78 @@
+import os
+import time
+import sys
+
+PDF = '.pdf'
+
+# Annotations
+SITES = ''.join([
+    '-e "s/www.it-ebooks.info/ /" ',
+    '-e "s/www.allitebooks.com/ /" ',
+])
+
+
+def install_pdftk():
+    """
+    If sys.platform is linux then install pdftk using apt.
+    """
+    os.system('sudo apt update')
+    os.system('sudo apt install pdftk')
+
+
+def clean(file_name):
+    """
+    Correctify name of file for pdftk.
+    """
+    file_name = file_name.replace(' ', '_') \
+        .replace('(', '') \
+        .replace(')', '') \
+        .replace(',', '') \
+        .replace('-', '-') \
+        .replace('!', '')
+
+    return file_name
+
+
+def rename(path):
+    """
+    Rename file.
+    """
+    for file in os.listdir(path):
+        if file.endswith(PDF):
+            if ' ' in file:
+                os.rename(file, clean(file))
+
+
+def finalize(file):
+    os.remove('{file}'.format(file=file))
+    os.remove('{file}1'.format(file=file))
+    os.rename('{file}2'.format(file=file), '{file}'.format(file=file))
+    print('\033[92mAnnotations removed from file\033[0m: \033[33m{0}\033[0m.'.format(file))
+
+
+def remove_annots(path):
+    """
+    Remove annotations from file.
+    """
+    rename(path)
+    cmd = 'pdftk {file} output {file}1 uncompress'
+    cmd_sed = 'sed {sites} {file}1 > {file}2'
+
+    for file in os.listdir(path):
+        if file.endswith(PDF):
+            os.system(cmd.format(file=file))
+            os.system(cmd_sed.format(file=file, sites=SITES))
+
+            time.sleep(0.5)
+            finalize(file)
+
+
+if __name__ == '__main__':
+    aw = input('Are you sure? [y/n]: ')
+    if aw.lower() in ['y', 'yes']:
+
+        if sys.platform == 'linux':
+            # install_pdftk()
+            pass
+
+        remove_annots(os.curdir)
